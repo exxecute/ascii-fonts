7130485d5e18deb1156c8eafbb07f164d66afecf
execute <kalich.vac@gmail.com>
2022-07-10 14:05:32 +0300

    complited font
    complited font 8x5

--- fonts.h
+++ fonts.h
@@ -2,18 +2,16 @@
 #ifndef INC_FONTS_H_
 #define INC_FONTS_H_
 
-#include "defines.h"
-#include "main.h"
-
 typedef struct {
 	const unsigned char *ascii;
 	const uint8_t height;
 	const uint8_t width;
 }font;
 
-extern font font8;
+extern const uint8_t ascii_table_font8x5[][8];
 
-font* Font_GetFont(uint8_t Font);
-uint8_t Font_GetPage(uint8_t Font, uint8_t Letter, uint8_t height);
+extern font font8x5;
+
+font* Font_GetFont8x5(void);
 
 #endif /* INC_FONTS_H_ */
